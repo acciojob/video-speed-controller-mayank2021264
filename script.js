59129d31c4616f73e1d826913be815623b676135
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-17 17:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-const inputs = document.querySelectorAll('.controls input');
+let video = document.querySelector('.player_video');
+let progress = document.querySelector('.progress');
+let progress_filled = document.querySelector('.progress__filled');
+let toggle = document.querySelector('.toggle');
+let range = document.querySelectorAll('input');
+let skip = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay(){
+	if(video.paused){
+		video.play();
+	}
+	else{
+		video.pause();
+	}
+}
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+	toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+video.addEventListener('pause', updateButton);
+video.addEventListener('play', updateButton);
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+range.forEach(input => input.addEventListener('input', handleRangeUpdate));
+
+function skipbtn() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+skip.forEach(btn => btn.addEventListener('click', skipbtn));
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = percent + '%';
+}
+video.addEventListener('timeupdate', handleProgress);
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+progress.addEventListener('click', scrub);
+
+
+
+
+
